531cc13de93dec9bad8aa112496ecefef25d1cb9
Dongmin Jin <dongminjin@Dongmins-MacBook-Pro-361.local>
2023-02-21 16:38:47 -0500

    permutation test and p values for the ranking

--- scripts/pydeseq2_permutation_gene.py
+++ scripts/pydeseq2_permutation_gene.py
@@ -1,15 +1,7 @@
-#! /usr/bin/env python
-
-import pandas as pd
-from pydeseq2.dds import DeseqDataSet
-from pydeseq2.ds import DeseqStats
-from pydeseq2.utils import load_example_data
-import numpy as np
 import pydeseq2
-from sys import argv
-import pandas as pd
-def pydeseq2_permutation_test(counts, group_labels, num_permutations=1000):
-    """Permutation test for differential abundance using PyDESeq2.
+
+def pydeseq2_permutation_test(counts, group_labels, num_permutations=10):
+    """Permutation test for differential expression using PyDESeq2.
 
     Parameters
     ----------
@@ -31,59 +23,54 @@ def pydeseq2_permutation_test(counts, group_labels, num_permutations=1000):
     group_labels,
     design_factors="disease",  # compare samples based on the "disease"
     # column ("B" vs "A")
-    refit_cooks=True,
-    n_cpus=8,
+    refit_cooks=True#,
+    #n_cpus=8,
 )
     dds.deseq2()
     stat_res = DeseqStats(dds, n_cpus=8)
     # Get the test statistics for each gene
     res = stat_res.summary()
-    
     #extract the log2FoldChange for each feature
     log2_fold_changes = stat_res.results_df["log2FoldChange"]
     #sort the log2foldchanges in descending order
     sorted_indices = np.argsort(log2_fold_changes)[::-1]
     sorted_log2_fold_changes = log2_fold_changes[sorted_indices]
     #calculate the rank of each gene
-    ranks = np.argsort(sorted_indices)
+    ranks = np.argsort(sorted_log2_fold_changes)
 
-    #TO DO initialize the null distribution of the ranking
+    #initialize the null distribution of the ranking
     null_distribution = np.zeros((num_permutations, counts.shape[1]))
 
     # Perform the permutations
     for i in range(num_permutations):
         # Shuffle the group labels
-#         permuted_labels = np.random.permutation(group_labels)
-#         permuted_labels = pd.DataFrame(permuted_labels)  
         permuted_labels = np.random.permutation(group_labels['disease'])
         permuted_labels = pd.DataFrame(permuted_labels, index=group_labels.index)
         permuted_labels = pd.DataFrame(permuted_labels)
         permuted_labels.columns = ['disease']
         # Convert the permuted group labels to PyDESeq2 input format
         # Run the DESeq2 analysis with the permuted group labels
-        counts.reindex(permuted_labels.index)
+        permuted_labels.reindex(counts.index)
+        #counts.reindex(permuted_labels.index)
         permuted_dds = DeseqDataSet(
         counts, 
         permuted_labels, 
         design_factors="disease")
         permuted_dds.deseq2()
-        permuted_stat_res = DeseqStats(permuted_dds, n_cpus=8)
+        permuted_stat_res = DeseqStats(permuted_dds)#, n_cpus=8)
         # Get the test statistics for each gene with the permuted group labels
         permuted_res = permuted_stat_res.summary()
-        log2_fold_changes_permutated = permuted_res.results_df["log2FoldChange"]
+        log2_fold_changes_permutated = permuted_stat_res.results_df["log2FoldChange"]
+        sorted_indices_permutated = np.argsort(log2_fold_changes_permutated)[::-1]
+        sorted_log2_fold_changes_permutated = log2_fold_changes_permutated[sorted_indices_permutated]
+        ranks_permutated_ori = np.argsort(sorted_log2_fold_changes_permutated)
+        ranks_permutated = ranks_permutated_ori.reindex(ranks.index)
+        #add ranks to the null distribution
+        null_distribution[i,:] = ranks_permutated
 
-    #TO DO: calculate the p values
-    #p values based on ranks
-    return p_values_ranks, stat_res.results_df
+    #TO DO: calculate the p values based on ranks
+    p_values_rank = np.zeros(counts.shape[1])
+    for i in range(counts.shape[1]):
+        p_values_rank[i] = np.sum(null_distribution[:, i] > ranks[i]) / num_permutations
 
-input_1 = argv[1]
-input_2 = argv[2]
-output_1 = argv[3]
-output_2 = argv[4]
-
-input_table = pd.read_table(input_1, sep = '\t', index_col = 0)
-input_table2 = pd.read_table(input_2, sep = '\t', index_col = 0)
-a,b = pydeseq2_permutation_test(input_table,input_table2)
-#a.to_csv(output_1)
-np.savetxt(output_1,a,delimiter=",")
-b.to_csv(output_2, sep = '\t')
+    return p_values_rank, stat_res.results_df
