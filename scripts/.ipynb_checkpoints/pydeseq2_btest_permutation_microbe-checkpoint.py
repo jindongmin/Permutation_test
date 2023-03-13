6974d04296361fb86fcd0422c6bd3d33f3beafd2
Dongmin Jin <dongminjin@Dongmins-MacBook-Pro-361.local>
2023-03-13 11:06:24 -0400

    filter out kegg with only 0 counts in bot and top gene matrix

--- scripts/.ipynb_checkpoints/pydeseq2_btest_permutation_microbe-checkpoint.py
+++ scripts/.ipynb_checkpoints/pydeseq2_btest_permutation_microbe-checkpoint.py
@@ -34,8 +34,9 @@ def btest(pa1, pa2, seed=0, return_proportions=False):
     pa1 = pa1.sum(axis=0).reindex(idx).fillna(0)
     pa2 = pa2.sum(axis=0).reindex(idx).fillna(0)
     n = pa1 + pa2
+    #print("min n", min(list(n.values)))
     obs = list(zip(list(pa1.values), list((pa2.values + 1) / (pa2 + 1).sum()), list(n.values)))
-    pvals = pd.Series([binom_test(a, n, b, 'two-sided') for (a, b, n) in obs],
+    pvals = pd.Series([binom_test(_a, _n, _b, 'two-sided') for (_a, _b, _n) in obs],
                       index=n.index)
     if return_proportions:
         res = pd.DataFrame({'groupA': pa1, 'groupB': pa2, 'pval': pvals})
@@ -93,25 +94,37 @@ def get_kegg_from_annotation(ids):
     return kegg_counts
  
     
-def _test(X, y, k, M):
+def _test(X, y, z, k, M, G, nc):
     """
     X: count matrix of microbes (m microbes X n samples)
     y: disease lables for samples (n samples)
+    z: colname of the condition (eg. disease status)
     k: top k microbes, eg k = 100
     M: metadata = pd.read_table('../table/eggNOG_species_rep.txt')
+    G: annotation matrix of microbes (microbe ids X kegg ids)
+    nc: number of cpu
     """
+    
     # Convert counts and group labels to PyDESeq2 input format
     dds = DeseqDataSet(
         X, 
         y, 
-        design_factors = "disease", # compare samples based on the "disease"
+        design_factors = z,# compare samples based on the "disease"
         refit_cooks = True,
-        n_cpus = 8
+        n_cpus = nc
     )
     dds.deseq2()
-    stat_res = DeseqStats(dds, n_cpus=8)
+    #set of the sample status: Healthy, disease
+    status = set(y[z])
+    #remove healthy
+    status.remove("Healthy")
+    disease = ', '.join(status)
+    #sample status colname: disease, 
+    #disease: name of that disease
+    stat_res = DeseqStats(dds,contrast = [z, disease, "Healthy" ], n_cpus=nc)
+    #stat_res = DeseqStats(dds,contrast = ["disease", "Healthy", "AD" ], n_cpus=8)
     res = stat_res.summary()
-    res_df = stat_res.results_df
+    res_df = stat_res.results_df    
     res_df["CI_95"] = res_df["log2FoldChange"] + res_df['lfcSE'] * 1.96
     res_df["CI_5" ] = res_df["log2FoldChange"] - res_df['lfcSE'] * 1.96
     #top k microbes: the ones with largest CI_5
@@ -127,11 +140,14 @@ def _test(X, y, k, M):
     top_id = [_test_top_id for _test_top_id in top_id if "." not in _test_top_id]
     bot_id = bot_rep['Species_rep'].drop_duplicates()
     bot_id = [_test_bot_id for _test_bot_id in bot_id if "." not in _test_bot_id]
-    #wget all ids: dir: /mnt/home/djin/ceph/Permutation_test/table/Species
-    #get G gene matrix
-    top_gene_matrix = get_kegg_from_annotation(top_id)
-    bot_gene_matrix = get_kegg_from_annotation(bot_id)
-
+    #get top and bot G gene matrix
+    top_gene_matrix = G.filter(items = top_id, axis=0)
+    bot_gene_matrix = G.filter(items = bot_id, axis=0)
+    frames = [top_gene_matrix, bot_gene_matrix]
+    Conta_gene_matrix = pd.concat(frames)
+    Conta_gene_matrix = Conta_gene_matrix.loc[:, (Conta_gene_matrix != 0).any(axis=0)]
+    top_gene_matrix = Conta_gene_matrix.filter(items = top_id, axis=0)
+    bot_gene_matrix = Conta_gene_matrix.filter(items = bot_id, axis=0)
     #binomial test Return number of genes elevated in top
     """
     C = btest(G[top], G[bot])
@@ -144,19 +160,19 @@ def _test(X, y, k, M):
     return C    
 
 
-def permutation_test(X, y, k, p, M):
+def permutation_test(X, y, z, k, p, M, G, nc):
     """
     p: number of permutations, eg p = 1000
     """
-    T = _test(X, y, k, M)
+    T = _test(X, y, z, k, M, G, nc)
     T_list = [0] * p
     for i in range(p):
         #shuffle the group lables
-        y_permutated = np.random.permutation(y['disease'])
+        y_permutated = np.random.permutation(y[z])
         y_permutated = pd.DataFrame(y_permutated, index=y.index)
-        y_permutated.columns = ['disease']
+        y_permutated.columns = [z]
         y_permutated.reindex(X.index)
-        T_ = _test(X, y_permutated, k, M)
+        T_ = _test(X, y_permutated, z, k, M, G, nc)
         T_list[i] = T_
         p_value = np.sum(T_list[i] > T) / (p+1)
     return T, p_value
@@ -170,17 +186,23 @@ def get_options():
     parser.add_argument("-y", dest="disease_labels",
                        help="disease lables for samples (n samples)",
                        required=True)
+    parser.add_argument("-z", dest="disease_status",
+                       help="colnames of the diseases status",
+                       required=True)
     parser.add_argument("-k", dest="top_k", default=100, type=int,
                        help="top k microbes. "
-                            "Default: %(default)i"
-                       )
+                            "Default: %(default)i")
     parser.add_argument("-p", dest="permutations", default=1000, type=int,
                        help="number of permutations. "
-                            "Default: %(default)i"
-                       )
+                            "Default: %(default)i")
     parser.add_argument("-M", dest="metadata",
                        help="metadata for ids and species",
                        required=True)
+    parser.add_argument("-G", dest="gene_matrix",
+                       help="gene matrix for microbes",
+                       required=True)
+    parser.add_argument("-nc", dest="number_cpus",default=8, type=int,
+                       help="number of cpus")
     parser.add_argument("-o", dest="output_file",
                        help="number of genes and p value",
                        required=True)
@@ -202,12 +224,16 @@ def main():
     input_table = pd.read_table(option.microbe_table, sep = '\t', index_col = 0)
     input_table2 = pd.read_table(option.disease_labels, sep = '\t', index_col = 'featureid')
     input_table3 = pd.read_table(option.metadata, sep = '\t')
+    input_table4 = pd.read_table(option.gene_matrix, sep='\t', index_col = 0 )
 
     a,b = permutation_test(X = input_table,
                            y = input_table2,
+                           z = option.disease_status,
                            k = option.top_k,
                            p = option.permutations,
-                           M = input_table3)
+                           M = input_table3,
+                           G = input_table4,
+                           nc = option.number_cpus)
 
     with open(option.output_file, "w") as output_h:
         output_h.write(str(a) + "\n")
