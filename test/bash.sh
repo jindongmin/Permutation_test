1f99a91e2442962da6ea60a6d8ca2ba8dac05dc6
Dongmin Jin <dongminjin@Dongmins-MacBook-Pro-361.local>
2023-03-14 13:46:28 -0400

    test simulated data

---
+++ test/bash.sh
@@ -0,0 +1,8 @@
+python ../scripts/pydeseq2_btest_permutation_microbe.py -x table_sim_microbes.txt \
+                                             -y metadata_sim.txt \
+                                             -z 'disease' \
+                                             -k 2 \
+                                             -p 10 \
+                                             -G G_matrix_sim.txt \
+                                             -pt 0.05 \
+                                             -o test_output.txt
